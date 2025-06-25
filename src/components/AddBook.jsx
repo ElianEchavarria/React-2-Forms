e52479908ee69164f7dedbb2e162572e7ae4f098
RamsesCode <RamsesSanchez25@gmail.com>
2025-06-25 15:36:39 -0400

    final commit

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -57,11 +57,11 @@ const AddBook = ({ onAdd, books }) => {
     <form onSubmit={handleSubmit} className="add-book-form">
       <input name="title" placeholder="Title" value={form.title} onChange={handleChange} required />
       <input name="author" placeholder="Author" value={form.author} onChange={handleChange} required />
-      <input name="image" placeholder="Image URL" value={form.image} onChange={handleChange} />
-      <input type="date" name="publishedDate" value={form.publishedDate} onChange={handleChange} />
-      <textarea name="description" placeholder="Description" value={form.description} onChange={handleChange} />
-      <input name="rating" type="number" min="1" max="5" placeholder="Rating (1-5)" value={form.rating} onChange={handleChange} />
-      <select name="category" value={form.category} onChange={handleChange}>
+      <input required name="image" placeholder="Image URL" value={form.image} onChange={handleChange} />
+      <input required type="date" name="publishedDate" value={form.publishedDate} onChange={handleChange} />
+      <textarea required name="description" placeholder="Description" value={form.description} onChange={handleChange} />
+      <input required name="rating" type="number" min="1" max="5" placeholder="Rating (1-5)" value={form.rating} onChange={handleChange} />
+      <select required name="category" value={form.category} onChange={handleChange}>
         <option value="">Select category</option>
         <option value="fiction">Fiction</option>
         <option value="non-fiction">Non-fiction</option>
